72eeaaa6573ec82f4f4b041a4a8618a02c79d0d9
SFJ5008 <a3123336874@126.com>
2026-06-15 23:58:44 +0800

    一些优化

--- constants.py
+++ constants.py
@@ -8,6 +8,7 @@ SUB_CNT_THRESHOLD = 1000
 SUCCESS: str = "SUCCESS: "
 ERROR: str = "ERROR: "
 LOG_SWITCH: bool = True
+USE_MAX_BUFFER_SIZE: int = 100 * 1024 * 1024 # 100MB
 
 type Result = tuple[Any, str]
     
